2d7c4f66181587b3ca840cdcb0737c33c7c9843e
PENCIL-TT <hariharantt015@gmail.com>
2025-08-31 17:01:23 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,8 +41,8 @@ const Footer = () => {
           </form>
         </div>
 
-        {/* ===== MAIN: 3 Columns ===== */}
-        <div className="grid grid-cols-1 gap-8 border-t border-white/10 py-12 md:grid-cols-2 lg:grid-cols-3">
+        {/* ===== MAIN: 2 Columns ===== */}
+        <div className="grid grid-cols-1 gap-8 border-t border-white/10 py-12 md:grid-cols-2">
           {/* Left: About */}
           <div className="space-y-4">
             <h4 className="text-lg font-semibold">About Moltech</h4>
@@ -53,7 +53,7 @@ const Footer = () => {
             </p>
           </div>
 
-          {/* Middle: Quick Links (2 columns) */}
+          {/* Right: Quick Links (2 columns) */}
           <div className="mx-auto w-full max-w-md">
             <h4 className="text-lg text-center font-semibold">Quick Links</h4>
             <ul className="mt-4 grid grid-cols-2 gap-x-8 gap-y-2 text-center">
@@ -69,91 +69,92 @@ const Footer = () => {
               ))}
             </ul>
           </div>
+        </div>
 
-          {/* Right: Global Offices */}
-          <div className="space-y-4">
-            <h4 className="text-lg font-semibold text-right lg:text-left">Our Global Presence</h4>
-            <div className="max-h-80 overflow-y-auto space-y-4 text-sm">
-              
-              {/* Australia */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">AUSTRALIA</h5>
-                <p className="text-white/85">Suite 5, 7-9 Mallet Road,<br />Tullamarine, Victoria, 3043</p>
-                <p className="text-white/85">+61 388205157</p>
-                <p className="text-white/85">janak@moltechglobal.com</p>
-              </div>
+        {/* ===== GLOBAL OFFICES SECTION ===== */}
+        <div className="border-t border-white/10 py-8">
+          <div className="text-center mb-8">
+            <h4 className="text-xl md:text-2xl font-semibold">Our Global Presence</h4>
+          </div>
+          
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            {/* Australia */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">AUSTRALIA</h5>
+              <p className="text-white/85 text-sm mb-1">Suite 5, 7-9 Mallet Road,<br />Tullamarine, Victoria, 3043</p>
+              <p className="text-white/85 text-sm mb-1">+61 388205157</p>
+              <p className="text-white/85 text-sm">janak@moltechglobal.com</p>
+            </div>
 
-              {/* Indonesia */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">INDONESIA</h5>
-                <p className="text-white/85">408, Lina Building, JL.HR Rasuna Said kav B7, Jakarta</p>
-                <p className="text-white/85">+62 815 1038 5581</p>
-                <p className="text-white/85">sourcing@moltechglobal.com</p>
-              </div>
+            {/* Indonesia */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">INDONESIA</h5>
+              <p className="text-white/85 text-sm mb-1">408, Lina Building, JL.HR Rasuna Said kav B7, Jakarta</p>
+              <p className="text-white/85 text-sm mb-1">+62 815 1038 5581</p>
+              <p className="text-white/85 text-sm">sourcing@moltechglobal.com</p>
+            </div>
 
-              {/* Malaysia - Portlang */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">MALAYSIA - PORTLANG</h5>
-                <p className="text-white/85">18 Jalan Sungai Chandong 12, Bdr Armada Putra<br />Pulau Indah, 42000 Pelabuhan Klang,<br />Selangor Darul Ehsan, Malaysia</p>
-                <p className="text-white/85">+60 16-985 4705</p>
-                <p className="text-white/85">arun@moltechglobal.com</p>
-              </div>
+            {/* Malaysia - Portlang */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">MALAYSIA - PORTLANG</h5>
+              <p className="text-white/85 text-sm mb-1">18 Jalan Sungai Chandong 12, Bdr Armada Putra<br />Pulau Indah, 42000 Pelabuhan Klang,<br />Selangor Darul Ehsan, Malaysia</p>
+              <p className="text-white/85 text-sm mb-1">+60 16-985 4705</p>
+              <p className="text-white/85 text-sm">arun@moltechglobal.com</p>
+            </div>
 
-              {/* Malaysia - Johor */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">MALAYSIA - JOHOR</h5>
-                <p className="text-white/85">HS(D) 576585 PTD 233430 No.19A, Jalan Sagai 6,<br />Taman Pasir Putih, 81700 Pasir Gudang, Johor</p>
-                <p className="text-white/85">+60 16-959 4075</p>
-                <p className="text-white/85">linda@moltechglobal.com</p>
-              </div>
+            {/* Malaysia - Johor */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">MALAYSIA - JOHOR</h5>
+              <p className="text-white/85 text-sm mb-1">HS(D) 576585 PTD 233430 No.19A, Jalan Sagai 6,<br />Taman Pasir Putih, 81700 Pasir Gudang, Johor</p>
+              <p className="text-white/85 text-sm mb-1">+60 16-959 4075</p>
+              <p className="text-white/85 text-sm">linda@moltechglobal.com</p>
+            </div>
 
-              {/* Saudi Arabia */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">SAUDI ARABIA - DAMMAM</h5>
-                <p className="text-white/85">2817 King Faizal Road, Dammam 9403-32233,<br />Kingdom of Saudi Arabia</p>
-                <p className="text-white/85">+966 13 3430003</p>
-                <p className="text-white/85">james@moltechglobal.com</p>
-              </div>
+            {/* Saudi Arabia */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">SAUDI ARABIA - DAMMAM</h5>
+              <p className="text-white/85 text-sm mb-1">2817 King Faizal Road, Dammam 9403-32233,<br />Kingdom of Saudi Arabia</p>
+              <p className="text-white/85 text-sm mb-1">+966 13 3430003</p>
+              <p className="text-white/85 text-sm">james@moltechglobal.com</p>
+            </div>
 
-              {/* Singapore */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">SINGAPORE</h5>
-                <p className="text-white/85">Blk 511 Kampong Bahru Road<br />#03-01 Keppel Distripark<br />Singapore 099447</p>
-                <p className="text-white/85">+65 65140868</p>
-                <p className="text-white/85">jenny@moltechglobal.com</p>
-              </div>
+            {/* Singapore */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">SINGAPORE</h5>
+              <p className="text-white/85 text-sm mb-1">Blk 511 Kampong Bahru Road<br />#03-01 Keppel Distripark<br />Singapore 099447</p>
+              <p className="text-white/85 text-sm mb-1">+65 65140868</p>
+              <p className="text-white/85 text-sm">jenny@moltechglobal.com</p>
+            </div>
 
-              {/* Thailand */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">THAILAND - BANGKOK</h5>
-                <p className="text-white/85">109 CCT Building, 3rd Floor, Rm.3,<br />Surawong Road, Suriyawongse, Bangrak,<br />Bangkok 10500</p>
-                <p className="text-white/85">+60 16-985 4705</p>
-                <p className="text-white/85">info@moltechglobal.com</p>
-              </div>
+            {/* Thailand */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">THAILAND - BANGKOK</h5>
+              <p className="text-white/85 text-sm mb-1">109 CCT Building, 3rd Floor, Rm.3,<br />Surawong Road, Suriyawongse, Bangrak,<br />Bangkok 10500</p>
+              <p className="text-white/85 text-sm mb-1">+60 16-985 4705</p>
+              <p className="text-white/85 text-sm">info@moltechglobal.com</p>
+            </div>
 
-              {/* UAE */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">UAE - DUBAI</h5>
-                <p className="text-white/85">Plot #2430152, Al Qusais Industrial Area 2,<br />Dubai, United Arab Emirates</p>
-                <p className="text-white/85">+971 509093357</p>
-                <p className="text-white/85">info@moltechglobal.com</p>
-              </div>
+            {/* UAE */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">UAE - DUBAI</h5>
+              <p className="text-white/85 text-sm mb-1">Plot #2430152, Al Qusais Industrial Area 2,<br />Dubai, United Arab Emirates</p>
+              <p className="text-white/85 text-sm mb-1">+971 509093357</p>
+              <p className="text-white/85 text-sm">info@moltechglobal.com</p>
+            </div>
 
-              {/* UK */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">UK - LONDON</h5>
-                <p className="text-white/85">167-169 Great Portland Street, 5th Floor,<br />London, W1W 5PF, United Kingdom</p>
-                <p className="text-white/85">+44(0) 7305 856612<br />+44(0) 203 393 9508</p>
-              </div>
+            {/* UK */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">UK - LONDON</h5>
+              <p className="text-white/85 text-sm mb-1">167-169 Great Portland Street, 5th Floor,<br />London, W1W 5PF, United Kingdom</p>
+              <p className="text-white/85 text-sm mb-1">+44(0) 7305 856612<br />+44(0) 203 393 9508</p>
+            </div>
 
-              {/* USA */}
-              <div className="border-l-2 border-[#F47E2A] pl-3">
-                <h5 className="font-semibold text-white">USA - NEW YORK</h5>
-                <p className="text-white/85">New Jersey Branch, 33 Wood Avenue South,<br />Suite 600, Iselin, NJ 08830</p>
-                <p className="text-white/85">+1 732 456 6780</p>
-                <p className="text-white/85">info@moltechglobal.com</p>
-              </div>
-
+            {/* USA */}
+            <div className="border-l-2 border-[#F47E2A] pl-4 bg-white/5 p-4 rounded-r-lg">
+              <h5 className="font-semibold text-white mb-2">USA - NEW YORK</h5>
+              <p className="text-white/85 text-sm mb-1">New Jersey Branch, 33 Wood Avenue South,<br />Suite 600, Iselin, NJ 08830</p>
+              <p className="text-white/85 text-sm mb-1">+1 732 456 6780</p>
+              <p className="text-white/85 text-sm">info@moltechglobal.com</p>
             </div>
           </div>
         </div>
