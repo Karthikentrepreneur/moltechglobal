bbacdcf49f0c765a2533017ec80f96d4f01b4a6a
PENCIL-TT <hariharantt015@gmail.com>
2025-09-02 18:22:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "@/pages/Index";
 import About from "@/pages/About";
-
 import ProductsGrid from "@/pages/ProductsGrid";
 import BiodieselFeedstocks from "@/pages/BiodieselFeedstocks";
 import FattyAcids from "@/pages/FattyAcids";
@@ -9,31 +8,28 @@ import SoapNoodles from "@/pages/SoapNoodles";
 import AnimalFeedFats from "@/pages/AnimalFeedFats";
 import Glycerin from "@/pages/Glycerin";
 import FeedAdditives from "@/pages/FeedAdditives";
-import Contact from "@/pages/Contact";
 import Global from "@/pages/Global";
-import Blog from "@/pages/blog";
+import Careers from "@/pages/Careers";
+import Blog from "@/pages/Blog";
+import Contact from "@/pages/Contact";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Main pages */}
         <Route path="/" element={<Index />} />
         <Route path="/about" element={<About />} />
- <Route path="/blog" element={<Blog />} />
-        {/* Products */}
         <Route path="/products" element={<ProductsGrid />} />
+        <Route path="/global" element={<Global />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/products/biodiesel-feedstocks" element={<BiodieselFeedstocks />} />
         <Route path="/products/fatty-acids" element={<FattyAcids />} />
         <Route path="/products/soap-noodles" element={<SoapNoodles />} />
         <Route path="/products/animal-feed-fats" element={<AnimalFeedFats />} />
         <Route path="/products/glycerin" element={<Glycerin />} />
         <Route path="/products/feed-additives" element={<FeedAdditives />} />
-
-        {/* Contact & Global Presence */}
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/global" element={<Global />} />
-
       </Routes>
     </Router>
   );
