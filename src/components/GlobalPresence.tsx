7f323a142b7bb7611efc217637ba1bc2341e6666
PENCIL-TT <hariharantt015@gmail.com>
2025-09-04 17:59:29 +0530

    Update GlobalPresence.tsx

--- src/components/GlobalPresence.tsx
+++ src/components/GlobalPresence.tsx
@@ -20,7 +20,7 @@ const GlobalPresence = () => {
       y: "68%",
       city: "Jakarta",
       address: "408, Lina Building, JL.HR Rasuna Said kav B7, Jakarta",
-      mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253840.65789016097!2d106.69!3d-6.211!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3e945e34b9d%3A0x5371bf0fdad786a2!2sJakarta%2C%20Indonesia!5e0!3m2!1sen!2s!4v1234567890"
+      mapUrl: "https://www.google.com/maps/d/embed?mid=1pph8JzHyVH3jfVQHvjnJW_Q4r1P7_7Q&ehbc=2E312F" 
     },
     { 
       name: "Malaysia", 
