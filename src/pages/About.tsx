c44160d8221bdf2c91a3eda0082d333e4ce0d7f0
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-11-18 00:22:58 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -102,7 +102,7 @@ const About: React.FC = () => {
             <div className="lg:col-span-6">
               <div className="rounded-2xl overflow-hidden shadow-[0_20px_60px_rgba(2,6,23,0.10)] ring-1 ring-black/5">
                 <img
-                  src="/aboutmoltech.jpg"
+                  
                   alt=""
                   className="w-full h-[380px] md:h-[440px] object-cover"
                 />
