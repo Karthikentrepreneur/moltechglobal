df4abaf4b6b5f4ed09eb82363eda99cf75f9c0c8
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-11-17 23:21:18 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,7 +14,7 @@ import {
 } from "lucide-react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import blogs1 from "../components/blogs1";
+import Blogs1 from "../components/Blogs1";
 
 /* -------------------- CountUp -------------------- */
 function easeOutCubic(t: number) {
