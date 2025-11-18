d69d58d5b1d8963d9ec0fdb67f3f9c37425e0fa7
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-11-18 21:25:21 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ const MESSAGES: Message[] = [
   },
 ];
 
-const SLIDE_MS = 5000;
+const SLIDE_MS = 10000;
 const FADE_MS = 500;
 
 const Hero = () => {
@@ -64,7 +64,7 @@ const Hero = () => {
     >
       {/* Background Video */}
       <video
-        src="/herohero.mp4"
+        src="/hre.mp4"
         autoPlay
         muted
         loop
